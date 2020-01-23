5818d104b2c0c3ddaf660639e6f0c3da05bfba2e
jonas <jonas.masharqa@gmail.com>
2020-01-23 17:30:49 +0100

    Adds cors to express server.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 let http = require('http').createServer(app)
 let io = require('socket.io')(http)
+
+app.use(cors());
 
 io.on('connection', client => {
   console.log('a user connected')
