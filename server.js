741196e1aded89a96be1e47b6627279bc43f08d4
jonas <jonas.masharqa@gmail.com>
2020-01-23 16:29:45 +0100

    Adds http server and bidirectional connection with ws.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+let http = require('http').createServer(app)
+let io = require('socket.io')(http)
 
+io.on('connection', client => {
+  console.log('a user connected')
 
-app.get('/server', (req, res) => {
-    res.send('response')
+  client.on('chat', data => {
+    console.log('Message received -->', data)
+
+    io.emit('chat', data)
+  })
 })
 
-app.listen(5000, () => {
-    console.log('listening ...')
+io.listen(5000, () => {
+  console.log('Listening ... 🚀 ')
 })
